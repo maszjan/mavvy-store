f33734a3cfd59c928ec198e20ed8e007a92a8996
adi <ma4shitm@gmail.com>
2025-10-10 12:05:09 +0200

    added meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,36 +12,64 @@ import { Analytics } from "@vercel/analytics/next";
 const { SITE_NAME } = process.env;
 
 export const metadata = {
-	metadataBase: new URL(baseUrl),
-	title: {
-		default: SITE_NAME!,
-		template: `%s | ${SITE_NAME}`,
-	},
-	robots: {
-		follow: true,
-		index: true,
-	},
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: SITE_NAME!,
+    template: `%s | ${SITE_NAME}`,
+  },
+  robots: {
+    follow: true,
+    index: true,
+  },
 };
 
 export default async function RootLayout({
-	children,
+  children,
 }: {
-	children: ReactNode;
+  children: ReactNode;
 }) {
-	const cart = getCart();
+  const cart = getCart();
 
-	return (
-		<html lang='en' className={GeistSans.variable}>
-			<body className='bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white'>
-				<CartProvider cartPromise={cart}>
-					<Navbar />
-					<main>
-						{children}
-						<Toaster closeButton />
-					</main>
-					<Analytics />
-				</CartProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={GeistSans.variable}>
+      <head>
+        {/* Facebook Meta Pixel */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src='https://connect.facebook.net/en_US/fbevents.js';
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script');
+            fbq('init', '750552071338373');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
+      <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
+        {/* Facebook Meta Pixel noscript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=750552071338373&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        <CartProvider cartPromise={cart}>
+          <Navbar />
+          <main>
+            {children}
+            <Toaster closeButton />
+          </main>
+          <Analytics />
+        </CartProvider>
+      </body>
+    </html>
+  );
 }
